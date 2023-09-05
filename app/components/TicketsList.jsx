34658b897a4d11ca47f1983b2692b3f0f0333d09
DevJuma10 <juma.study@gmail.com>
2023-09-05 18:39:22 +0300

    Tickets Done

--- app/components/TicketsList.jsx
+++ app/components/TicketsList.jsx
@@ -1,54 +1,95 @@
-// const url = "https://www.boredapi.com/api/activity";
-const url = "http://localhost:4000/tickets";
+// // const url = "https://www.boredapi.com/api/activity";
+// const url = "http://localhost:4000/tickets";
 // const url = 'http://localhost:4000/tickets'
 
+// // async function getTickets() {
+// //     fetch(url, {
+// //                 method: 'get',
+// //                 mode: 'cors'
+// //     })
+// //         .then((response) => {
+// //             return response.json()})
+// //         .catch((err) => console.log(err));
+// // }
+
+// ////////////////////////////////GOLD///////////////////////////////////////
+// // let tickets = fetch(url)
+// //     .then((response) => response.json())
+// //     .then(data => {
+// //         console.log(data)
+
+// //         return data;
+// //     })
+// //     .catch(error => {
+// //         return error;
+// //     });
+
 // async function getTickets() {
-//     fetch(url, {
-//                 method: 'get',
-//                 mode: 'cors'
-//     })
-//         .then((response) => {
-//             return response.json()})
-//         .catch((err) => console.log(err));
+//     try {
+//       const response = await fetch(url);
+//       if (!response.ok) {
+//         throw new Error('Network response was not ok');
+//       }
+//       const data = await response.json();
+//       return data;
+//     } catch (error) {
+//       throw error;
+//     }
+//   }
+  
+//   getTickets()
+//   .then((tickets) => {
+//     console.log(tickets);
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
+  
+
+// async function getTickets(){
+//     const res = await fetch('http://localhost:4000/tickets')
+//     const data = await res.json();
+//     return await data
 // }
 
 
-let tickets = fetch(url)
-    .then((response) => response.json())
-    .then(data => {
+async function getTickets() {
+    try {
+      const response = await fetch('http://localhost:4000/tickets');
+      if (!response.ok) {
+        console.error(`Network error: ${response.status} - ${response.statusText}`);
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  }
+  
 
-        
-        const finalData = Object.values(data.tickets)
-        
-        console.log(typeof(finalData))
-        console.log(finalData);
-        
-
-        return finalData;
-    })
-    .catch(error => {
-        return error;
-    });
-
-    
-    const TicketsList = () => {
-  return (
-    <>
-    {/* {tickets.map( (ticket) => {
-        <div key={ticket.id} className="card my-5">
-            <h3>{ticket.title}</h3>
-            <p>{ticket.body.slice(0,200)}</p>
-            <div className={`pill ${ticket.proprity}`}>
-            {ticket.priority} participants
+  export default async function TicketsList() {
+    try {
+      const tickets = await getTickets();
+  
+      return (
+        <>
+          {tickets.map((ticket) => (
+            <div key={ticket.id} className="card my-5">
+              <h3>{ticket.title}</h3>
+              <p>{ticket.body.slice(0, 200)}...</p>
+              <div className={`pill ${ticket.priority}`}>
+                {ticket.priority} priority
+              </div>
             </div>
-
-        </div>
-    } )}
-    {tickets.length === 0 && (
-        <p className="text-center">There are no open tickets</p>
-    ) } */}
-    </>
-  )
-}
-
-export default TicketsList
+          ))}
+          {tickets.length === 0 && (
+            <p className="text-center">There are no open tickets</p>
+          )}
+        </>
+      );
+    } catch (error) {
+      // Handle the error here, e.g., show an error message or log it.
+      console.error(error);
+    }
+  }
