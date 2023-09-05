67757eb1ec4400a2c6fa15916e61894b0cb773ad
DevJuma10 <juma.study@gmail.com>
2023-09-05 16:51:32 +0300

    Handling caching

--- app/components/TicketsList.jsx
+++ app/components/TicketsList.jsx
@@ -1,37 +1,52 @@
-const url = "https://randomuser.me/api/"
+// const url = "https://www.boredapi.com/api/activity";
+const url = "http://localhost:4000/tickets";
 // const url = 'http://localhost:4000/tickets'
 
-async function getTickets() {
-    fetch(url, {
-                method: 'get',
-                mode: 'cors'
+// async function getTickets() {
+//     fetch(url, {
+//                 method: 'get',
+//                 mode: 'cors'
+//     })
+//         .then((response) => {
+//             return response.json()})
+//         .catch((err) => console.log(err));
+// }
+
+
+let tickets = fetch(url)
+    .then((response) => response.json())
+    .then(data => {
+
+        
+        const finalData = Object.values(data.tickets)
+        
+        console.log(typeof(finalData))
+        console.log(finalData);
+        
+
+        return finalData;
     })
-        .then((response) => {
-            return response.json()})
-        .catch((err) => console.log(err));
-}
+    .catch(error => {
+        return error;
+    });
 
-const TicketsList = async () => {
-
-    const tickets = await getTickets();
-    console.log("/////////////////////////////////////////////////////////")
-    console.log(tickets)
-
+    
+    const TicketsList = () => {
   return (
     <>
-    {tickets.map( (ticket) => {
+    {/* {tickets.map( (ticket) => {
         <div key={ticket.id} className="card my-5">
             <h3>{ticket.title}</h3>
             <p>{ticket.body.slice(0,200)}</p>
-            <div className={`pill ${ticket.priority}`}>
-            {ticket.priority} priority
+            <div className={`pill ${ticket.proprity}`}>
+            {ticket.priority} participants
             </div>
 
         </div>
     } )}
     {tickets.length === 0 && (
         <p className="text-center">There are no open tickets</p>
-    ) }
+    ) } */}
     </>
   )
 }
