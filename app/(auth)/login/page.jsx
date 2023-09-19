ce62af3f2eff06a5644c08a2917d5bd61805242e
DevJuma10 <juma.study@gmail.com>
2023-09-19 14:29:20 +0300

    Added Signup/Login pages

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,8 +1,18 @@
+"use client"
+import AuthForm from "../AuthForm";
 
 export default function Login() {
+
+  const handleSubmit = async (e, email, password) => {
+    e.preventDefault()
+
+    console.table("User Login",email, password)
+  }
+
   return (
     <main>
         <h2>Login</h2>
+        <AuthForm handleSubmit={handleSubmit} />
     </main>
   )
 }
--- app/(auth)/signup/page.jsx
+++ app/(auth)/signup/page.jsx
@@ -1,7 +1,19 @@
+"use client"
+
+import AuthForm from "../AuthForm";
+
 export default function SignUp() {
+
+  const handleSubmit = async (e, email, password) => {
+    e.preventDefault()
+
+    console.table('User Sign-up', email, password)
+  }
+
   return (
    <main>
     <h2>SignUp</h2>
+    < AuthForm handleSubmit={handleSubmit}/>
    </main>
   )
 }
