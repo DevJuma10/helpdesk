f34bfb10097c3793cb628aae21bcd6aadc4d45cc
DevJuma10 <juma.study@gmail.com>
2023-09-25 10:23:00 +0300

    feat(Getting Current User) :Get Current User
    Get Current user from the session cookies and dispaly logged in user email on navbar

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,21 +1,22 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import Logo from '../../public/logo.png'
-const Navbar = () => {
+
+
+export default function Navbar( { user }) {
   return (
     <nav>
-        <Image 
-          src={Logo}
-          alt='helpdesk logo'
-          width={70}
-          quality={100}
-          placeholder='blur'
-          />
-      <h1>Helpdesk</h1>
-      <Link href="/">Dashboard</Link>
-      <Link href="/ticket">Tickets</Link>
-    </nav>
+    <Image 
+      src={Logo}
+      alt='helpdesk logo'
+      width={70}
+      quality={100}
+      placeholder='blur'
+      />
+  <h1>Helpdesk</h1>
+  <Link href="/">Dashboard</Link>
+  <Link href="/ticket">Tickets</Link>
+  {user && <span>Hello, {user.email} </span>}
+</nav>
   )
 }
-
-export default Navbar
