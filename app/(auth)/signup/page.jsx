adda1127ea6a94e8806a79c32406f425929855b4
DevJuma10 <juma.study@gmail.com>
2023-09-25 09:58:35 +0300

    FEATURE(SIGNUP): ADD SIGNUP
    Add Signup and signup routes using supabase

--- app/(auth)/signup/page.jsx
+++ app/(auth)/signup/page.jsx
@@ -1,11 +1,37 @@
 "use client"
 
+import { useState } from "react";
 import AuthForm from "../AuthForm";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
 
 export default function SignUp() {
+  const router = useRouter()
+
+const [error, setError] = useState("")
 
   const handleSubmit = async (e, email, password) => {
     e.preventDefault()
+    setError("")
+
+    const superbase = createClientComponentClient()
+    const {error} = await superbase.auth.signUp({
+      email,
+      password,
+      options: {
+        emailRedirectTo: `${location.origin}/api/auth/callback`
+      }
+    })
+
+    if(error){
+        setError(error.message)
+    }
+
+    if(!error){
+      //redirec user
+      router.push('/verify')
+
+    }
 
     console.table('User Sign-up', email, password)
   }
@@ -14,6 +40,9 @@ export default function SignUp() {
    <main>
     <h2>SignUp</h2>
     < AuthForm handleSubmit={handleSubmit}/>
+    {error && (
+      <div className="error">{error}</div>
+    )}
    </main>
   )
 }
---
+++ app/(auth)/verify/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Verify() {
+  return (
+    <main className="text-center">
+        <h2>Thanks for registering!</h2>
+        <p>before loggin in, please verify your email address. </p>
+    </main>
+  )
+}
