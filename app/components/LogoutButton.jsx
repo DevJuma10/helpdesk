c168a1b8722045cb3c14b8b2250a687936e75e8f
DevJuma10 <juma.study@gmail.com>
2023-09-26 13:19:09 +0300

    Fix: Logout Function
    Fix bug on logout handler allowing redirect user to /login after succesful logout form a session

--- app/components/LogoutButton.jsx
+++ app/components/LogoutButton.jsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation"
 
 export default function LogoutButton() {
 
-    const router = useRouter
+    const router = useRouter()
 
     const handleLogout = async () => {
         const supabase = createClientComponentClient()
